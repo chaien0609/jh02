676bc5e8ebf48ce0ec593f6839d319e8fe3efe14
cuongnh0609 <cuongnh0609@gmail.com>
2020-05-26 16:46:12 +0900

    initial commit

---
+++ src/main/java/jh02/service/InvalidPasswordException.java
@@ -0,0 +1,11 @@
+package jh02.service;
+
+public class InvalidPasswordException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public InvalidPasswordException() {
+        super("Incorrect password");
+    }
+
+}
---
+++ src/main/java/jh02/service/UsernameAlreadyUsedException.java
@@ -0,0 +1,11 @@
+package jh02.service;
+
+public class UsernameAlreadyUsedException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public UsernameAlreadyUsedException() {
+        super("Login name already used!");
+    }
+
+}
